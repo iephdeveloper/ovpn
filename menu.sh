1f98a6eba093a2faae2558efbbd371269c5fcc42
iephdeveloper <44111129+iephdeveloper@users.noreply.github.com>
2018-10-15 18:36:42 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Menu
+
+echo -e "Welcome to IEPH DEVELOPERS SCRIPT"| lolcat
+
+echo -e "* menu       : Displays a list of available commands"
+echo -e "* usernew    : Creating OpenVPN Account"
+echo -e "* delete     : Clearing OpenVPN Account"
+echo -e "* member     : Check Member OpenVPN"
+echo -e "* restart    : Restart Service OpenVPN"
+echo -e "* reboot     : reboot VPS"
+echo -e "* speedtest  : speedtest VPS"
+echo -e "* info       : System Information"
+echo -e "* exit       : exit Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e ""
